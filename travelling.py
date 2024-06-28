121906264c9cbf3e1c9006df6aad646c51a0f356
Olivia0511 <gxchilin511@gmail.com>
2024-06-29 00:01:25 +0800

    travelling.py

--- travelling.py
+++ travelling.py
@@ -18,9 +18,29 @@ def find_optimal_route(capitals):
     return route
 # Coordinates of cities
 capitals = [
-    (42.3601, -71.0589),  # Washington, DC
-    (41.8781, -93.0977),  # Des Moines, IA
-    # other cities
+    (),  # Washington, DC
+    (41.590833, -93.620833)  # Des Moines, IA
     
+    (32.3675, -86.3), #Montgomery, Alabama
+    (58.3, -134.416111) #Juneau, Alaska
+    (33.448333, -112.073889) # Phoenix, Arizona
+    (34.736111, -92.331111) #Little Rock, Arkansas
+    (38.581667, -121.494444) #Sacramento, California
+    (39.7392, -104.9849) # Denver, Colorado
+    (41.7625, -72.674167) # Hartford, Connecticut
+    (39.158056, -75.524444) # Dover, Delaware
+    (30.438333, -84.280556) # Tallahassee, Florida
+    (33.748889, -84.39)# Atlanta, Georgia
+    (21.306944, -157.858333) # Honolulu,Hawaii
+    (43.615833, -116.201667) #Boise County, Idaho
+    (39.798333, -89.675833) #Springfield, Illinois
+    (39.768611, -86.158056) #Indianapolis,  Indiana
+    (39.034722, -95.695556) #Topeka, Kansas
+    (38.2, -84.866667) # Frankfort, Kentucky
     
 ]
+
+optimal_route = find_optimal_route(capitals)
+
+for i, city in enumerate(optimal_route):
+    print("Step {i+1}: {city}")
